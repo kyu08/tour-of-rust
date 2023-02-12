4fd2f54ba0e20735e64998770d47e50cd1df7ac6
kyu08 <49891479+kyu08@users.noreply.github.com>
2023-02-13 02:28:45 +0900

    - 同じ変数名への再割り当ては可能 - 変数名にはsnake_caseを使う

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    // x の型を推論
+    let x = 13;
+    println!("{}", x);
+
+    let x: f64 = 3.14;
+    println!("{}", x);
+
+    let x;
+    x = 0;
+    println!("{}", x);
 }
