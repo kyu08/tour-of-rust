5b42fee429e94e0a4ce6db100e4049fcc4719ca6
kyu08 <49891479+kyu08@users.noreply.github.com>
2023-02-13 02:33:37 +0900

    mutってつけるとmutable

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,6 @@
 fn main() {
-    // x の型を推論
-    let x = 13;
+    let mut x = 54;
     println!("{}", x);
-
-    let x: f64 = 3.14;
-    println!("{}", x);
-
-    let x;
-    x = 0;
+    x = 13;
     println!("{}", x);
 }
