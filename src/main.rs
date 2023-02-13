4866f260447c3cc712a1515956ce52b3d96969fc
kyu08 <49891479+kyu08@users.noreply.github.com>
2023-02-13 23:19:45 +0900

    ..と..=

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 fn main() {
-    let (x, y) = tuple_func(1);
-    println!("x:{}, y: {}", x, y);
-}
-
-fn tuple_func(x: i32) -> (i32, i32) {
-    (x, x + 1)
+    for x in 0..=5 {
+        println!("{}", x);
+    }
 }
