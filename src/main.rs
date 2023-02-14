0475dc23937d6a62d79744af38f328ed3d403a32
kyu08 <49891479+kyu08@users.noreply.github.com>
2023-02-15 01:59:27 +0900

    - 列挙型のメモリサイズはそれが持つ最大要素のサイズと等しい - 要素の型以外に、各要素には数字値がついておりどのタグであるかについて示している

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,29 @@
-fn main() {
-    let loc = Location(1, 2);
+#![allow(dead_code)] // この行でコンパイラのwaringsメッセージを止める
+
+enum Species {
+    Crab,
+    Octopus,
+    Fish,
+    Clam,
 }
 
-struct Location(i32, i32);
+struct SeaCreature {
+    species: Species,
+    name: String,
+    age: i32,
+}
+
+fn main() {
+    let ferris = SeaCreature {
+        species: Species::Crab,
+        name: String::from("ferris"),
+        age: 3,
+    };
+
+    match ferris.species {
+        Species::Crab => println!("crab"),
+        Species::Octopus => println!("octopus"),
+        Species::Fish => println!("fish"),
+        Species::Clam => println!("clam"),
+    }
+}
