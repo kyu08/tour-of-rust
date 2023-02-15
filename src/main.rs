d77c97e7ec6375890c36f5e62a5091478f1aaa08
kyu08 <49891479+kyu08@users.noreply.github.com>
2023-02-16 01:06:20 +0900

    result?; とするといい感じに取り出せる

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
-fn main() {
-    match int_to_result(100) {
-        Ok(a) => println!("{}円つかっていいよ", a),
-        Err(e) => println!("{}", e),
-    }
+fn main() -> Result<(), String> {
+    let age = 19;
+    let v = int_to_result(age)?;
+    println!("{}", v);
+    Ok(())
 }
 
 fn int_to_result(age: i32) -> Result<i32, String> {
-    let number = 18;
-    if age > number {
+    if age > 18 {
         return Ok(12);
     }
     Err(String::from("you are 未成年！"))
