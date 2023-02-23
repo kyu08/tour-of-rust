36a076104262f64ee610ffee63886bc0a553b925
kyu08 <49891479+kyu08@users.noreply.github.com>
2023-02-23 16:23:29 +0900

    traitは実装を持つことができる。traitを実装した構造体側で上書きすることもできる

--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,24 @@ impl User {
 
 trait Runner {
     fn run(&self);
-    fn say(&self);
+    fn say(&self) {
+        print!("hello");
+    }
 }
 
 impl Runner for User {
     fn run(&self) {
         println!("{} is running", self.get_name())
     }
-    fn say(&self) {}
+    fn say(&self) {
+        println!("hi")
+    }
 }
 
 fn main() {
     let u = User {
         name: String::from("john"),
     };
-    u.run();
+    // u.run();
+    u.say();
 }
