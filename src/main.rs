5ad1a9d2621d48914dd5a4c353907bf04fe1240e
kyu08 <49891479+kyu08@users.noreply.github.com>
2023-02-23 16:22:12 +0900

    trait

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,28 @@
+struct User {
+    name: String,
+}
+
+impl User {
+    fn get_name(&self) -> &str {
+        &self.name
+    }
+}
+
+trait Runner {
+    fn run(&self);
+    fn say(&self);
+}
+
+impl Runner for User {
+    fn run(&self) {
+        println!("{} is running", self.get_name())
+    }
+    fn say(&self) {}
+}
+
 fn main() {
-    let chars = "hi".chars().collect::<Vec<char>>();
-    println!("{}", chars.len());
-    println!("{}", chars[3] as u32);
+    let u = User {
+        name: String::from("john"),
+    };
+    u.run();
 }
